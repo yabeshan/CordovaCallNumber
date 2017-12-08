87eaac4e915a188522bc4cdc0344206c00aa8772
beshan <yabeshan@ya.ru>
2017-12-08 16:58:09 +0200

    change plugin

--- node_modules/call-number/src/android/CFCallNumber.java
+++ node_modules/call-number/src/android/CFCallNumber.java
@@ -1,4 +1,4 @@
-package mx.ferreyra.callnumber;
+package com.rohithvaranasi.callnumber;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -32,10 +32,16 @@ public class CFCallNumber extends CordovaPlugin {
     this.callbackContext = callbackContext;
     this.executeArgs = args;
 
-    if (cordova.hasPermission(CALL_PHONE)) {
-      callPhone(executeArgs);
+    if (action.equals("callNumber")) {
+      if (cordova.hasPermission(CALL_PHONE)) {
+        callPhone(executeArgs);
+      } else {
+        getCallPermission(CALL_REQ_CODE);
+      }
+    } else if (action.equals("isCallSupported")) {
+        this.callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, isTelephonyEnabled()));
     } else {
-      getCallPermission(CALL_REQ_CODE);
+      return false;
     }
 
     return true;
